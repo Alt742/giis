9a13625e0218033c5ccf28cc8c99052d4aec1b49
Denis Vorobev <30407902+Alt742@users.noreply.github.com>
2026-02-18 14:09:13 +0300

    Fix CheckGetContractorResponse XML namespace mapping

--- giis/src/main/java/org/webvane/giis/dmdk/request/dmdk/elements/response/CheckGetContractorResponseData.java
+++ giis/src/main/java/org/webvane/giis/dmdk/request/dmdk/elements/response/CheckGetContractorResponseData.java
@@ -6,7 +6,7 @@ import javax.xml.bind.annotation.XmlElement;
 import java.util.List;
 
 public class CheckGetContractorResponseData  extends ResponseData {
-    @XmlElement(name = "result", namespace = "urn://xsd.dmdk.goznak.ru/exchange/3.0")
+    @XmlElement(name = "result", namespace = "urn://xsd.benemed/exchange/1.0")
     private List<ResultContractor> result;
     public List<ResultContractor> getResult() {
         return this.result;
